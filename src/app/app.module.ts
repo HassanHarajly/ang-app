924d109f8adf80f52bb5ef2fdd4db005eeba4298
unknown <hassanmharajly@hotmail.com>
2020-05-02 23:35:58 -0400

    hharajl1 pushing component intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroPageComponent } from './components/intro-page/intro-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroPageComponent
   ],
   imports: [
     BrowserModule
