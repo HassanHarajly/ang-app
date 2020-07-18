70dfaffadca0d09444f66303d880525ac5ddf6cc
unknown <hassanmharajly@hotmail.com>
2020-07-18 17:40:04 -0400

    hharajl1 adding image to start personal page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IntroPageComponent } from './components/intro-page/intro-page.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroPageComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/intro-page/intro-page.component.ts
+++ src/app/components/intro-page/intro-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-intro-page',
   templateUrl: './intro-page.component.html',
